77f646a1f8782910440b2a423d479e96e331c855
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-02-03 11:40:28 -0600

    update entire page theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import About from './About';
+import Experience from './Experience';
+import Projects from './Projects';
+import LLM from './LLM';
+import Navbar from './Navbar';
+import './styles.css'; 
+import '@fontsource/roboto/300.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className='bg-[#111418]'>
+        < Navbar />
+        <div className='flex w-full'>
+          {/* <About />  */}
+          <LLM />
+        </div>
+        <Experience /> 
+        <Projects />
+        <div className='justify-center flex pb-12'>
+          <img className="" src='footer.svg' alt='footer' />
+        </div>
+      </div> 
+    </body>
   );
 }
 
