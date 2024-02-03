ae24b202a434ef89648f6cb72b49e5cf8a3c779e
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-02-03 11:40:37 -0600

    add internship experience

---
+++ src/Experience.js
@@ -0,0 +1,15 @@
+function Experience() {
+    return (
+        <div className="px-40 pb-40">
+            <div className="text-white text-4xl pb-20">Experience.</div>
+            <div className="flex gap-x-4">
+                <img src="card1.svg" alt="card" />
+                <img src="card2.svg" alt="card" />
+                <img src="card3.svg" alt="card" />
+                <img src="card4.svg" alt="card" />
+            </div>
+        </div>
+    );
+}
+
+export default Experience;
