ada08ee2e873b293a330e145bf60c52f7d3cc732
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-02-03 11:40:19 -0600

    create about section

---
+++ src/About.js
@@ -0,0 +1,23 @@
+// import './index.css'; 
+
+// function About() {
+//     return  (
+//         <>
+//         <div className="w-full py-72 pl-40">
+//             <div className="text-5xl z-5 text-white">Aishwarya Sudarshan</div>
+//             <div className="text-2xl pt-4 z-5 text-[#5197F7]">B.S. Computer Science</div>
+//             <p className="text-lg pt-16 z-5 text-[#AEB5C0] w-1/2">
+//                 Hi! I’m currently a junior at the University of Texas at Dallas pursuing a B.S. in Computer Science. I am interested in the intersection between AI and healthcare! 
+//             </p>
+//             <div className='pt-4 gap-x-2 flex'>
+//                 <img src="github1.png" className='w-8 h-8' alt='github'></img>
+//                 <img src="linkedin.png" className='w-8 h-8' alt='linkedin'></img>
+//             </div>
+//         </div> 
+//         {/* <img src='../blob2.png' alt='blob2' className='opacity-50'/>        */}
+//         </>
+//     );
+
+// }
+
+// export default About;
