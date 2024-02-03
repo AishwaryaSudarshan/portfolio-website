93516c890ceca3c67e42ffd8b5a6621e309e54c3
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-02-03 11:41:20 -0600

    add links to projects and info

---
+++ src/Projects.js
@@ -0,0 +1,135 @@
+
+
+function Projects() {
+    return (
+        <div className="px-40 pb-40">
+            <div className="text-white text-4xl pb-20">Projects.</div>
+            <div className="flex gap-x-12 w-full">
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-36">
+                        <div className="text-white text-xl font-bold p-6">TrendSent</div>
+                        <a className="pt-8" href="https://devpost.com/software/trendsent">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                    <div className="px-6 text-[#A6ADB8]">A machine learning model built to predict the projected valuation a company in the industry using previous company financial data, company's competitor financial reports, sentiment analysis, and LLMs. This project won an award by placing 3rd in the Goldman Sachs Sponsor Challenge at a hackathon - HackUTD 2023. </div>
+                    <div className="flex px-6 pt-6 gap-x-4">
+                        <div className="text-[#78B0F9] text-sm">LLM</div>
+                        <div className="text-[#78B0F9] text-sm">Sentimental Analysis</div>
+                        <div className="text-[#78B0F9] text-sm">ML</div>
+                    </div>
+                </div>
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-24">
+                        <div className="text-white text-xl font-bold p-6">Drowsy Driving</div>
+                        <a className="pt-8" href="https://devpost.com/software/elevator-pitch">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                    <div className="px-6 pb-2 text-[#A6ADB8]">A hardware/facial recognition project that aims to prevent casualties by alarming drivers if they fall asleep at the wheel. This project won an award by placing 15/200 in a Toyota Sponsor Challenge at a hackathon - HackUTD 2022.</div>
+                    <div className="flex px-6 pt-16 gap-x-4">
+                        <div className="text-[#78B0F9] text-sm">Raspberry Pi</div>
+                        <div className="text-[#78B0F9] text-sm">Facial Recognition</div>
+                    </div>
+                </div>
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-36">
+                        <div className="text-white text-xl font-bold p-6">SnapCycle</div>
+                    </div> 
+                    <div className="px-6 text-[#A6ADB8]">An image classification app for users to accurately classify recyclable items based on three classifications. This project was created in an effort to minimize contributions to global warming and won awards in the UT Arlington Research Fair, AYSI App Challenge, and Science Fair Grand Prize at a Regional level. </div>
+                    <div className="flex px-6 pt-6 gap-x-4">
+                        <div className="text-[#78B0F9] text-sm">InceptionV3</div>
+                        <div className="text-[#78B0F9] text-sm">TFLite</div>
+                        <div className="text-[#78B0F9] text-sm">Android Studios</div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex gap-x-12 w-full pt-16">
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-28">
+                        <div className="text-white text-xl font-bold p-6">Foster Finder</div>
+                        <a className="pt-8" href="https://devpost.com/software/foster-finder-3w1ypz">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                    <div className="px-6 text-[#A6ADB8]">A frontend project aimed at providing a networking platform for foster parents to connect with agentsm organizations, and other foster parents. This project was created to participate in a hackathon, ShellHacks.</div>
+                    <div className="flex px-6 pt-24 gap-x-4">
+                        <div className="text-[#78B0F9] text-sm">Next JS</div>
+                        <div className="text-[#78B0F9] text-sm">React JS</div>
+                        <div className="text-[#78B0F9] text-sm">Firebase</div>
+                    </div>
+                </div>
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-6">
+                        <div className="text-white text-xl font-bold p-6">Women Who Compute</div>
+                        <a className="pt-8" href="https://www.utdwwc.com/">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                        <div className="px-6 text-[#A6ADB8] pb-2">A website created for a club focused on empowering women in the advancement of computer science and engineering. </div>
+                        <div className="flex px-6 pt-40 gap-x-4">
+                            <div className="text-[#78B0F9] text-sm">TypeScript</div>
+                            <div className="text-[#78B0F9] text-sm">Tailwind CSS</div>
+                        </div>
+                </div>
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-36">
+                        <div className="text-white text-xl font-bold p-6">AI Society</div>
+                        <a className="pt-8" href="https://www.aisutd.org/">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                        <div className="px-6 text-[#A6ADB8] pb-2">A website created for a club focused on making AI more understandable and accessible to students. Contributed to a page in the website as part as the tech development team.</div>
+                        <div className="flex px-6 pt-28 gap-x-4">
+                            <div className="text-[#78B0F9] text-sm">TypeScript</div>
+                            <div className="text-[#78B0F9] text-sm">React Animate</div>
+                        </div>
+                </div>
+            </div>
+            <div className="flex gap-x-12 w-full pt-16">
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-40">
+                        <div className="text-white text-xl font-bold p-6">HackAI</div>
+                        <a className="pt-8" href="https://www.hackai.org/">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                        <div className="px-6 text-[#A6ADB8]">A website created for a club hosted hackathon specializing in AI projects. Contributed to the website with dynamically updating sections.</div>
+                        <div className="flex px-6 pt-36 gap-x-4">
+                            <div className="text-[#78B0F9] text-sm">TypeScript</div>
+                            <div className="text-[#78B0F9] text-sm">Coda API</div>
+                        </div>
+                </div>
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-40">
+                        <div className="text-white text-xl font-bold p-6">Vaalve</div>
+                        <a className="pt-8" href="https://devpost.com/software/vaalve">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                        <div className="px-6 text-[#A6ADB8]">A frontend responsive website that suggests potential fatal heart conditions, if any, a user can have based off of their answers to certain health related questions. This project was created at a hackathon - WEHack. </div>
+                        <div className="flex px-6 pt-24 gap-x-4">
+                            <div className="text-[#78B0F9] text-sm">JavaScript</div>
+                            <div className="text-[#78B0F9] text-sm">HTML</div>
+                            <div className="text-[#78B0F9] text-sm">C++</div>
+                        </div>
+                </div>
+                <div className="w-1/3 h-96 bg-[#20262D] pt-2 rounded-3xl">
+                    <div className="flex gap-x-32">
+                        <div className="text-white text-xl font-bold p-6">Campanion</div>
+                        <a className="pt-8" href="https://devpost.com/software/campanion">
+                            <img className="" src="link3.svg" alt="link"/>
+                        </a>
+                    </div>
+                        <div className="px-6 pb-2 text-[#A6ADB8]">A frontend project that aimed to create a platform for campers to sign up for camping trips, connect with fellow campers, and document their entire experience through the app. This project won best visualization award in hackathon - HackBeanPot. </div>
+                        <div className="flex px-6 pt-16 gap-x-4">
+                            <div className="text-[#78B0F9] text-sm">Figma</div>
+                            <div className="text-[#78B0F9] text-sm">XCode</div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
